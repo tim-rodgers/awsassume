1529c378803b20cb0f7948dc6271c05c9ec72ac7
Khan <waqark3389@gmail.com>
2019-06-07 16:25:13 +0100

    export ps1 env var as the name of the profile being passed.

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -21,6 +21,7 @@ func EnvVars(credentials *awsassume.CredentialsValue, region string) (envVars []
 		fmt.Sprintf("AWS_SECRET_ACCESS_KEY=%s", credentials.SecretAccessKey),
 		fmt.Sprintf("AWS_SESSION_TOKEN=%s", credentials.SessionToken),
 		fmt.Sprintf("SESSION_EXPIRATION=%s", credentials.SessionExpiration),
+		fmt.Sprintf("PS1=%s", viper.GetString("Profile")+"> "),
 	)
 	if cRegion := viper.Get("Region"); cRegion != nil {
 		envVars = append([]string{fmt.Sprintf("AWS_DEFAULT_REGION=%s", cRegion)}, envVars...)
